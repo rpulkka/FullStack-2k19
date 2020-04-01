4d85b26d16a38909e2f37c23e3ec1f0ba9df5654
rpulkka <robert.pulkka@gmail.com>
2020-04-01 08:41:52 +0300

    Modifications for implementing Cypress.

--- Osa_4/blogilista/utils/config.js
+++ Osa_4/blogilista/utils/config.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 let SECRET = process.env.SECRET
+let MODE = process.env.NODE_ENV
 
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
@@ -11,5 +12,6 @@ if (process.env.NODE_ENV === 'test') {
 module.exports = {
   MONGODB_URI,
   PORT,
-  SECRET
+  SECRET,
+  MODE
 }
